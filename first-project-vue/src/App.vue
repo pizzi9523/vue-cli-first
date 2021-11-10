59a36d8c3cb261ce9a812fc2492ce0b19a99df08
pizzi9523 <pizzitolagiuseppe@live.it>
2021-11-10 14:56:51 +0100

    Creo Struttura e Aggiungo un titolo

---
+++ first-project-vue/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Title />
+  </div>
+</template>
+
+<script>
+import Title from "./components/Title.vue";
+
+export default {
+  name: "App",
+  components: {
+    Title,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
---
+++ first-project-vue/src/components/Title.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="title">
+    <h1>BENVENUTO NEL PRIMO PROGETTO</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Title",
+};
+</script>
+
+<style scoped>
+.title {
+  margin-top: 60px;
+  text-align: center;
+}
+</style>
