b15b9a3f100601e203294155e2434e468844aa78
pizzi9523 <pizzitolagiuseppe@live.it>
2021-11-10 16:09:00 +0100

    Bonus instal bootstrap Add jumbotron by bootstrap

---
+++ first-project-vue/src/components/Hero.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    class="
+      position-relative
+      overflow-hidden
+      p-3 p-md-5
+      m-md-3
+      text-center
+      bg-light
+      hero
+    "
+  >
+    <div class="col-md-5 p-lg-5 mx-auto my-5">
+      <h1 class="display-4 font-weight-normal text-light">Punny headline</h1>
+      <p class="lead font-weight-normal text-light">
+        And an even wittier subheading to boot. Jumpstart your marketing efforts
+        with this example based on Apple's marketing pages.
+      </p>
+      <a class="btn btn-warning" href="#">Coming soon</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.hero {
+  background-image: url("https://picsum.photos/id/1025/1920/1080");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: top;
+}
+</style>
--- first-project-vue/src/components/Title.vue
+++ first-project-vue/src/components/Title.vue
@@ -5,9 +5,7 @@
 </template>
 
 <script>
-export default {
-  name: "Title",
-};
+export default {};
 </script>
 
 <style scoped>
